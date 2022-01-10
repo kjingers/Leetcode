bfa93dd06ea7694bcd3a711686f66a52b2af5181
kjingers <kurtis.ingersoll@gmail.com>
2022-01-10 11:16:16 -0500

    Update LetterCasePermutations.py

--- Problems/LetterCasePermutations/LetterCasePermutations.py
+++ Problems/LetterCasePermutations/LetterCasePermutations.py
@@ -1,1 +1,34 @@
-Temp
+
+
+
+def find_letter_case_string_permutations(str):
+  permutations = []
+  n = len(str)
+
+  def dfs(index, curr):
+
+    # Base Case
+    if index == len(str):
+      permutations.append(''.join(curr))
+      return
+    
+    # Appending current Character as is
+    dfs(index + 1, curr + [str[index]])
+
+    # If character is alphanumeric, then append the other case
+    if str[index].isalpha():
+      dfs(index + 1, curr + [str[index].swapcase()])
+  
+  dfs(0, [])
+
+  return permutations
+
+
+def main():
+  print("String permutations are: " +
+        str(find_letter_case_string_permutations("ad52")))
+  print("String permutations are: " +
+        str(find_letter_case_string_permutations("ab7c")))
+
+
+main()
