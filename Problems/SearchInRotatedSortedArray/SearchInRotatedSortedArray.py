63e7ead25b0ef35b7590345d179c85531c939980
kjingers <kurtis.ingersoll@gmail.com>
2022-01-14 08:13:44 -0500

    Create SearchInRotatedSortedArray.py

---
+++ Problems/SearchInRotatedSortedArray/SearchInRotatedSortedArray.py
@@ -0,0 +1,41 @@
+def search_rotated_array(arr, key):
+  start, end = 0, len(arr) - 1
+  
+  # Start off like Normal, calculate Mid
+  while start <= end:
+    mid = start + (end - start) // 2
+
+    if key == arr[mid]:
+      return mid
+
+    # If arr[mid] >= arr[start], then we know start-->mid is 
+    # sorted in Ascending order.
+    if arr[mid] >= arr[start]:
+      
+      # Now we check if key is in this range.
+      if key >= arr[start] and key < arr[mid]:
+
+        # If in this range, adjust end
+        end = mid - 1
+
+      # Else, adjust start, since we know key is in second half
+      else:
+        start = mid + 1
+
+    else: # Else, mid-->end in ascending order
+      
+      if key > arr[mid] and key <= arr[end]:
+        start = mid + 1
+      
+      else:
+        end = mid - 1
+
+  return -1
+
+
+def main():
+  print(search_rotated_array([10, 15, 1, 3, 8], 15))
+  print(search_rotated_array([4, 5, 7, 9, 10, -1, 2], 10))
+
+
+main()
