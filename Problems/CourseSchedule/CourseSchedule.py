52bcb5124e34fcdbc876b815df44f07abb09d703
kjingers <kurtis.ingersoll@gmail.com>
2022-01-28 11:46:40 -0500

    Create CourseSchedule.py

---
+++ Problems/CourseSchedule/CourseSchedule.py
@@ -0,0 +1,43 @@
+from collections import deque
+
+
+def is_scheduling_possible(tasks, prerequisites):
+      sortedOrder = []
+      sources = deque()
+      graph = {i: [] for i in range(tasks)}
+      inDegrees = {i: 0 for i in range(tasks)}
+
+      # Build Graph and inDegrees
+      for parent, child in prerequisites:
+            graph[parent].append(child)
+            inDegrees[child] += 1
+      
+      # Add all sources to queue
+      for vertex, numInDegrees in inDegrees.items():
+            if numInDegrees == 0:
+                  sources.append(vertex)
+
+      # While sources: pop source, add to sorted order
+      # Decrement inDegrees. Add to sources if 0 inDegrees
+      while sources:
+            source = sources.popleft()
+            sortedOrder.append(source)
+            for vertex in graph[source]:
+                  inDegrees[vertex] -= 1
+                  if inDegrees[vertex] == 0:
+                        sources.append(vertex)
+      
+      
+      return tasks == len(sortedOrder)
+  
+
+
+def main():
+  print("Is scheduling possible: " +
+        str(is_scheduling_possible(3, [[0, 1], [1, 2]])))
+  print("Is scheduling possible: " +
+        str(is_scheduling_possible(3, [[0, 1], [1, 2], [2, 0]])))
+  print("Is scheduling possible: " +
+        str(is_scheduling_possible(6, [[0, 4], [1, 4], [3, 2], [1, 3]])))
+
+main()
