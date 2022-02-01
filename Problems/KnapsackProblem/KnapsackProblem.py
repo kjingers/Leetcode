65a7073fa35632b295549083c59e510473d8c5f7
kjingers <kurtis.ingersoll@gmail.com>
2022-02-01 12:25:04 -0500

    Update KnapsackProblem.py

--- Problems/KnapsackProblem/KnapsackProblem.py
+++ Problems/KnapsackProblem/KnapsackProblem.py
@@ -31,5 +31,31 @@ def knapsack(weights, prices, capacity):
   
   return solveKnapsack(weights, prices, capacity, 0)
   
+# With Memoization using (capacity, index) as key
 
-stressTesting = False
+def solveKnapsack(weights, prices, capacity, index, memo):
+
+  # Base Case. Out of capacity or no more objects
+  if capacity <= 0 or index >= len(weights):
+    return 0
+  if (capacity, index) in memo:
+    return memo[(capacity, index)]
+
+
+  # If weight of item at current index is > capacity, skip to next item
+  if weights[index] > capacity:
+    return solveKnapsack(weights, prices, capacity, index + 1, memo)
+
+  # Calculate total by calling recursively two functions
+  # 1. Pick item at current index
+  # 2. Do not pick item at current index
+  doPickItem = prices[index] + solveKnapsack(weights, prices, capacity - weights[index], index + 1, memo)
+  dontPickItem = solveKnapsack(weights, prices, capacity, index + 1, memo)
+
+  # We will return the max of picking vs not picking the item
+  memo[(capacity, index)] = max(doPickItem, dontPickItem)
+  return max(doPickItem, dontPickItem)
+
+def knapsack(weights, prices, capacity):
+  memo = {}
+  return solveKnapsack(weights, prices, capacity, 0, memo)
