d40920ac6b4b35292de847d54347d91568ee41b4
kjingers <kurtis.ingersoll@gmail.com>
2022-02-01 08:17:01 -0500

    Update Permutations.py

--- Problems/Permutations/Permutations.py
+++ Problems/Permutations/Permutations.py
@@ -1,28 +1,27 @@
 '''
-This is permutations of a string, but very similar to permutations of a list.
+This is permutations of a list.
+
+Since we are passing in a copy of currPerm + nums[i], we do not need to backttrack. If we appending and passed in reference to currPerm,
+we we would need to pop() after recursive call.
 
 Time Complexity: O(N!)
 - For a string with len of 3, we have 3 calls, each with 2 calls, each with 1 call. So 3x2x1 = 3!
 '''
-
-def permutations(str):
-  
-  output = []
-  permutations_rec(str, [], output)
-  
-  return output
-
-def permutations_rec(str, currPerm, output):
-
-  # Base Case: Empty String
-  if str == "":
-    output.append(''.join(currPerm))
-    return
-  
-  # For each character in string, "DFS" to get all permutations
-  # Of remaining characters after current character
-  # We are passing in copy of currPerm, so no backtracking necessary
-  for i in range(len(str)):
-    permutations_rec(str[:i] + str[i+1:], currPerm + [str[i]], output)
+    
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        output = []
+        self.permute_rec(nums, [], output)
+        return output
+        
+    def permute_rec(self, nums, currPerm, outputPerms):
+        
+        # Base Case (Empty list)
+        if not nums:
+            outputPerms.append(currPerm)
+            return
+        
+        for i in range(len(nums)):
+            self.permute_rec(nums[:i] + nums[i+1:], currPerm + [nums[i]], outputPerms)
 
 
