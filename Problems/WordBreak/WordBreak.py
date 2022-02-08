48615251183a882a21556a0e09ad4081b0c5b053
kjingers <kurtis.ingersoll@gmail.com>
2022-02-08 09:57:16 -0500

    Update WordBreak.py

--- Problems/WordBreak/WordBreak.py
+++ Problems/WordBreak/WordBreak.py
@@ -4,6 +4,10 @@ We can iterate through the string. At each index, check all words in wordDict to
 If a word ends at current index, we mark dp[i] = True, if dp[i - len(word)] == True. This means that
 a word ends at the current index, and also a previous word ended at the index before this word.
 
+Essentially, we start from 0 and build out dp[0] to dp[len(s) + 1].
+dp[i] is True when words from wordDict can be used to build s from index 0 to i. So, once we get to the end,
+if dp[i + 1] == True, that means words from wordDict can build all of s.
+
 Time Complexity: O(nk) where n is length of s and k is number of words in wordDict
 
 
