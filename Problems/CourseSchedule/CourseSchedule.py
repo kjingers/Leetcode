d65523bcef0d5688c26d32cfd0b8180e4a641b22
kjingers <kurtis.ingersoll@gmail.com>
2022-02-09 08:50:28 -0500

    Update CourseSchedule.py

--- Problems/CourseSchedule/CourseSchedule.py
+++ Problems/CourseSchedule/CourseSchedule.py
@@ -1,53 +1,51 @@
 '''
-Same as normal Topological Sort. 
+Topological Sort. If len(sortedList) == numCourses, then we can finish all the courses.
 
-If there is a cycle, then the length of the sorted order will not equal the number of tasks/vertices.
+Topological Sort:
+1. Initialize Graph and inDegrees
+2. Build Graph and inDegrees
+3. Add sources to queue
+4. For each source, for each of it's children:
+- Add to sortedOrder output
+- "Remove" Source by decrementing inDegrees for each child
+- If inDegrees is 0, then child is now a source so add to queue
+
+Time Complexity: O(V + E)
 '''
-
 from collections import deque
 
-def is_scheduling_possible(tasks, prerequisites):
-      sortedOrder = []
-      sources = deque()
-      graph = {i: [] for i in range(tasks)}
-      inDegrees = {i: 0 for i in range(tasks)}
-
-      # Build Graph and inDegrees
-      for parent, child in prerequisites:
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        # 1. Initialize graph and inDegrees
+        graph = {i:[] for i in range(numCourses)}
+        inDegrees = {i:0 for i in range(numCourses)}
+        
+        # 2. Build graph and inDegrees
+        for child, parent in prerequisites:
             graph[parent].append(child)
             inDegrees[child] += 1
-      
-      # Add all sources to queue
-      for vertex, numInDegrees in inDegrees.items():
-            if numInDegrees == 0:
-                  sources.append(vertex)
-
-      # While sources: pop source, add to sorted order
-      # Decrement inDegrees. Add to sources if 0 inDegrees
-      while sources:
-            source = sources.popleft()
-            sortedOrder.append(source)
-            for vertex in graph[source]:
-                  inDegrees[vertex] -= 1
-                  if inDegrees[vertex] == 0:
-                        sources.append(vertex)
-      
-      
-      #return tasks == len(sortedOrder)
-      if tasks == len(sortedOrder):
-            return sortedOrder
-      return []
-     
-
-  
-
-
-def main():
-  print("Is scheduling possible: " +
-        str(is_scheduling_possible(3, [[0, 1], [1, 2]])))
-  print("Is scheduling possible: " +
-        str(is_scheduling_possible(3, [[0, 1], [1, 2], [2, 0]])))
-  print("Is scheduling possible: " +
-        str(is_scheduling_possible(6, [[0, 4], [1, 4], [3, 2], [1, 3]])))
-
-main()
+            
+        # 3. Put sources in queue
+        sources = deque()
+        for vertex, val in inDegrees.items():
+            if val == 0:
+                sources.append(vertex)
+                
+        # 4. Go through sources, build sortedOrder
+        sortedOrder = []
+        while sources:
+            course = sources.popleft()
+            sortedOrder.append(course)
+            for child in graph[course]:
+                inDegrees[child] -= 1
+                if inDegrees[child] == 0:
+                    sources.append(child)
+                    
+        if len(sortedOrder) == numCourses:
+            return True
+    
+        return False
+                
+        
+        
