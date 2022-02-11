f49f3129028b95a7690997e48d7e10ab18508e34
kjingers <kurtis.ingersoll@gmail.com>
2022-02-11 09:07:03 -0500

    Update RemoveNthNodeFromEndOfList.py

--- Problems/RemoveNthNodeFromEndOfList/RemoveNthNodeFromEndOfList.py
+++ Problems/RemoveNthNodeFromEndOfList/RemoveNthNodeFromEndOfList.py
@@ -6,12 +6,44 @@
 
 
 '''
-1. Iterate though the list to get the length of the list
-2. Calculate len - n to get how many nodes we have to skip
-3. Remove node
+Get Length of Linked List.
+Then skip len - n nodes
+Remove the next node
 
-Time Complexity: O(n)
+
 '''
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        
+        # Get length
+        
+        curr = head
+        length = 0
+        
+        # Get length of Linked List
+        while curr:
+            curr = curr.next
+            length += 1
+         
+        curr = head
+        prev = None
+        
+        # Skip (length - n) nodes
+        # After loop, curr will be node to remove
+        for _ in range(length - n):
+            prev = curr
+            curr = curr.next
+            
+        # If prev is still None, then we need to remove head
+        if not prev:
+            newHead = head.next
+            head.next = None
+            return newHead
+        else:
+            prev.next = curr.next
+            curr.next = None
+            return head
+            
+            
+            
         
