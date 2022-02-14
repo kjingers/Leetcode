f5ada454ff91c7caf0a665a8de9cb63f083e755c
kjingers <kurtis.ingersoll@gmail.com>
2022-02-14 10:57:59 -0500

    Update LongestSubstringWithoutRepeatingCharacters.py

--- Problems/LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters.py
+++ Problems/LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharacters.py
@@ -3,6 +3,9 @@ Standard sliding window problem
     - Increase window until repeating character (window size > len(dict))
     - Shrink window until no repeating characters (above condition false)
     - Update global max
+    
+O(n) time
+O(k) --> O(1) Space. K is number of distint characters. So worst case O(26) -> O(1)
 '''
 
 class Solution:
@@ -12,25 +15,23 @@ class Solution:
             return 0
         
         maxLen = 0
-        currChars = {}
+        d = {}
         
         start = 0
         for end in range(len(s)):
             
-            if s[end] not in currChars:
-                currChars[s[end]] = 0
-                
-            currChars[s[end]] += 1
+            # We've seen this character before. If it was before our current window,
+            # then the start will be the same (no need to adjust). Else, we need
+            # to move start to last seen index + 1, so that s[end] is the only occurence of the letter
+            # in our window
             
-            while (end - start + 1) > len(currChars):
-                currChars[s[start]] -= 1
-                
-                if currChars[s[start]] == 0:
-                    del currChars[s[start]]
-                    
-                start += 1
+            if s[end] in d:
+                start = max(start, d[s[end]] + 1)
                 
             maxLen = max(maxLen, end - start + 1)
+            
+            d[s[end]] = end
+            
             
         return maxLen
             
