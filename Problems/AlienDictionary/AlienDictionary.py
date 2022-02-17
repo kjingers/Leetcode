145681503667fbb2618b3bc571b57db770670cf4
kjingers <kurtis.ingersoll@gmail.com>
2022-02-17 09:44:06 -0500

    Update AlienDictionary.py

--- Problems/AlienDictionary/AlienDictionary.py
+++ Problems/AlienDictionary/AlienDictionary.py
@@ -1,65 +1,62 @@
 '''
-Similar to typical topological sort problem. 
-Main difference is that we don't have the prerequisite pairs. To build the graph, we
-must compare all adjacent word pairs. Compare letter by letter until a letter is different.
-The different character in the left word is a parent, and the different character in the right
-word is the child.
-
+For this, we can to find a complete order of the alphabet. From the input sorted words, we can figure out
+the order of some of the letters. We can put these orders into a graph (adjacency list) and perform
+Topological Sort.
 '''
 
 from collections import deque
 
-def find_order(words):
-  sortedOrder = []
-  sources = deque()
-
-
-  # Initialize Graph and inDegrees
-  graph = {}
-  inDegrees = {}
-
-  # Make sure each distint character is a key in both maps
-  for word in words:
-    for char in word:
-      graph[char] = []
-      inDegrees[char] = 0
-
-  # Build Graph and inDegrees by comparing adjacent word pairs
-  for i in range(len(words)- 1):
-    w1, w2 = words[i], words[i+1]
-    for j in range(min(len(w1), len(w2))):
-      parent = w1[j]
-      child = w2[j]
-      if parent != child:
-        if child not in graph[parent]:
-          graph[parent].append(child)
-          inDegrees[child] += 1
-        break
-  
-  # Add Sources
-  for vertex in inDegrees:
-    if inDegrees[vertex] == 0:
-      sources.append(vertex)
-
-  # While sources, process each source
-  while sources:
-    source = sources.popleft()
-    sortedOrder.append(source)
-    for child in graph[source]:
-      inDegrees[child] -= 1
-      if inDegrees[child] == 0:
-        sources.append(child)
-
-  if len(sortedOrder) != len(graph):
-    return ""
-
-  return ''.join(sortedOrder)
-
-
-def main():
-  print("Character order: " + find_order(["ba", "bc", "ac", "cab"]))
-  print("Character order: " + find_order(["cab", "aaa", "aab"]))
-  print("Character order: " + find_order(["ywx", "wz", "xww", "xz", "zyy", "zwz"]))
-
-
-main()
+class Solution:
+    def alienOrder(self, words: List[str]) -> str:
+        
+        graph = {}
+        inDegrees = {}
+        sortedOrder = []
+        
+        # Initialize graph and inDegrees with a key for each distinct character
+        for word in words:
+            for char in word:
+                graph[char] = []
+                inDegrees[char] = 0
+                
+        for i in range(1, len(words)):
+            left = words[i - 1]
+            right = words[i]
+            
+            for j in range(min(len(left), len(right))):
+                if left[j] != right[j]:
+                    if right[j] not in graph[left[j]]:
+                        graph[left[j]].append(right[j])
+                        inDegrees[right[j]] += 1
+                    break
+                # If word2 is a subset of word1 (ex "abc, "ab"), then this cannot be
+                # a valid sorted order of words. return
+                elif j == min(len(left), len(right)) - 1 and len(left) > len(right):
+                    return ""
+                              
+        print(graph)
+        
+        # Put Sources into queue
+        sources = deque()
+        for vertex, inD in inDegrees.items():
+            if inD == 0:
+                sources.append(vertex)
+                
+        
+        while sources:
+            source = sources.popleft()
+            sortedOrder.append(source)
+            for child in graph[source]:
+                inDegrees[child] -= 1
+                if inDegrees[child] == 0:
+                    sources.append(child)
+                    
+        
+        return ''.join(sortedOrder) if len(sortedOrder) == len(graph) else ""
+            
+            
+        
+                
+        
+                
+        
