8d969393c63437d9b476ab838ccb9979f47bda76
kjingers <kurtis.ingersoll@gmail.com>
2022-02-17 12:45:21 -0500

    Update LowestCommonAncestorOfABinaryTreeIII.py

--- Problems/LowestCommonAncestorOfABinaryTreeIII/LowestCommonAncestorOfABinaryTreeIII.py
+++ Problems/LowestCommonAncestorOfABinaryTreeIII/LowestCommonAncestorOfABinaryTreeIII.py
@@ -13,6 +13,17 @@ Looks similar to original Lowest Common Ancestor of a binary tree. However, each
 So, we can skip the dfs/bfs to build the parent dictionary.
 
 O(n) worst case I think
+
+Another solution for O(1) space, is to find merge point similar to how you would in Linked List.
+- p's path to root is: a + c
+- q's path to root is: b + c
+Where c is the path after they have converged. a and b is the length of path to the convergence point for p and q respectively.
+
+So, we make both nodes travel the full distance:
+    p: a + c + b
+    q: b + c + a
+    
+These disctances are equivalent and both nodes are at the merge point, which  is also the LCA.
 '''
 
 class Solution:
