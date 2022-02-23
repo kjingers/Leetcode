6cabc0a8f647b70eb0d76627dbc579142a852be7
kjingers <kurtis.ingersoll@gmail.com>
2022-02-23 08:12:50 -0500

    Update TrappingRainWater.py

--- Problems/TrappingRainWater/TrappingRainWater.py
+++ Problems/TrappingRainWater/TrappingRainWater.py
@@ -5,7 +5,57 @@ water is trapped.
 Monotonic Decreasing Stack can be used to find next greater elevation.
 -   For each elevation we add without popping, we can add water to the current trap.
 -   One we find the next >= elevation, we can add the water to our output
+
+
+First did this problem with monotonic decreasing stack. 
+
+Another method is using left and right pointers.
+    - Maintain leftPointer, leftMax, rightPointer, leftMax
+    - If leftmax < rightMax, then iterate left pointer
+        - if height[left] >= leftMax, then just update leftMax. No water to add
+        - If height[left] < leftMax, then add water += leftMax - height[left]
+    - Same with decrementing right pointer
+    
+NOTE: left <= right to get the last water at the end.
+
+Since we move the side that is shorter, when current height is less than max, we are guarenteed to hold water,
+since we've seen a higher left end (leftMax) and know there is a higher right end (rightMax > leftMax)
 '''
+
+# Two Pointer Method
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        
+        if len(height) < 3:
+            return 0
+        
+        n = len(height)
+        
+        leftMax, rightMax = height[0], height[n - 1]
+        left, right = 1, n - 2
+        water = 0
+        
+        while left <= right:
+            if leftMax < rightMax:
+                if height[left] > leftMax:
+                    leftMax = height[left]
+                else:
+                    water += leftMax - height[left]
+                    
+                left += 1
+            
+            else:
+                if height[right] > rightMax:
+                    rightMax = height[right]
+                else:
+                    water += rightMax - height[right]
+                    
+                right -= 1
+                
+        return water
+
+
 
 from collections import deque
 
