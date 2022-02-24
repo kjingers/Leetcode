2701bb3e54aaaaa2b34e1b5334f8d13e5c1effc9
kjingers <kurtis.ingersoll@gmail.com>
2022-02-24 15:45:24 -0500

    Update JumpGameII.py

--- Problems/JumpGameII/JumpGameII.py
+++ Problems/JumpGameII/JumpGameII.py
@@ -12,6 +12,7 @@ Space: O(1)
 '''
 
 # Greedy
+
 class Solution:
     def jump(self, nums: List[int]) -> int:
         
@@ -31,3 +32,60 @@ class Solution:
             numJumps += 1
             
         return numJumps
+
+
+# Top-Down DP
+# dp[i] is the minimum number of jumps to each the end from index i
+'''
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        dp = [10001 for i in range(len(nums))]
+        return self.jump_rec(nums, 0, dp)
+        
+        
+    def jump_rec(self, nums, pos, dp):
+        
+        # Base Case
+        if pos >= len(nums) - 1:
+            return 0
+        
+        if dp[pos] != 10001:
+            return dp[pos]
+        
+        for j in range(1, nums[pos] + 1):
+            dp[pos] = min(dp[pos], 1 + self.jump_rec(nums, pos + j, dp))
+            
+        return dp[pos]
+'''
+# Bottom-up DP
+# dp[n - 1] = 0
+# Then loop backwards from n - 2 to 0.
+# For each i, Iterate j from 1 to nums[i]
+#   dp[i] = min(dp[i], 1 + dp[i + j])
+#   **Bounds handling, so we don't check beyond last index**
+#        dp[i] = min(dp[i], 1 + dp[min(i + j, n - 1)]
+'''
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n <= 1:
+            return 0
+        
+        dp = [10001 for i in range(n)]
+        dp[n - 1] = 0
+        for i in range(n - 2, -1, -1):
+            for j in range(1, nums[i] + 1):
+                dp[i] = min(dp[i], 1 + dp[min(i + j, n - 1)])
+                
+        return dp[0]
+        
+'''
+        
+        
+        
+        
+        
+        
+        
+        
+        
