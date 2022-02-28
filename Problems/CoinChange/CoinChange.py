963422176d7f1fcad4104d1a674a1d77788cf7f4
kjingers <kurtis.ingersoll@gmail.com>
2022-02-28 08:56:05 -0500

    Update CoinChange.py

--- Problems/CoinChange/CoinChange.py
+++ Problems/CoinChange/CoinChange.py
@@ -1,50 +1,101 @@
 '''
-Unbounded Knapsack problem, since we can use unlimited of each item.
-So at each index we have two options:
-1. Pick the current coin and recursively solve remaining total with same index
-2. Do not pick current coin and recursively solve remaining total with next index
+Unbounded Knapsack problem. Cannot use greedy, since picking the largest coin possible won't always give
+the correct answer.
 
-Can use memo with tuple as key.
-Or use 2D array: dp[# coins][total]
+Top Down w/ memoization:
+
+At each index and total, we can either:
+    1. If total - coins[i] >= 0, we can pick that coin and recursively call at the same index
+    2. Skip the current coin, and recursively call for all remaining coins
+    
+The minimum of the above two options is the minimum result to store in dp[i][T]
+
+** NOTE: Base case should return 0. We add one after recursive call (if successful).
+** Default c1 and fail case to inf, so we don't pic kthat for the min.
+
+Bottom up:
+
+dp[index][total] - Represents the Minimum number of coins needed to make total, using all coins up to index 'index'
+
+dp[index][total] = min(dp[index - 1][total], 1 + dp[index][total - coin[index]])
+    ** Must check if index > 0, and if total >= coins[i]
+
+dp[index][0] = 0 for all indexes, because 0 coins required to make total of 0
+
+Space Optimization:
+
+Instead of a 2D array, we can have dp[amount]. Our outer loop loops through the amounts. For each amount ,we loop through all the coins, and pick the min of dp[amount - coin] if coin >= amount
 '''
+
 import math
 
+# Bottom-up
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         
+        n = len(coins)
+        
+        # Initialize to math.inf, since we want the minimum
+        dp = [[math.inf for _ in range(amount + 1)] for _ in range(n)]
+        
+        # For call indexes, takes 0 coins to get total of 0
+        for i in range(n):
+            dp[i][0] = 0
+            
+            
+        for i in range(n):
+            for tot in range(1, amount + 1):
+                
+                # Skip ith coin
+                if i > 0:
+                    dp[i][tot] = dp[i - 1][tot]
+                
+                # Take ith coin
+                if coins[i] <= tot:
+                    dp[i][tot] = min(dp[i][tot], 1 + dp[i][tot - coins[i]])
+                    
+                       
+        return dp[n - 1][amount] if dp[n - 1][amount] != math.inf else -1
+                              
+
+
+# Top Down w/ Memoization
+'''
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
         dp = [[-1 for _ in range(amount + 1)] for _ in range(len(coins))]
-        result = self.coinChange_rec(coins, amount, 0, dp)
+        res = self.solve(coins, 0, amount, dp)
         
-        return -1 if result == math.inf else result
+        return -1 if res == float('inf') else res
         
-    
-    def coinChange_rec(self, coins, amount, index, dp):
+    def solve(self, coins, index, total, dp):
         
-        # Base Cases:
+        # Base cases
+        if index == len(coins):
+            return float('inf')
         
-        # If amount == 0, then the coins we picked satisfy the total
-        if amount == 0:
+        if dp[index][total] != -1:
+            return dp[index][total]
+        
+        if total == 0:
             return 0
         
-        # If we have no more coins, then there is not a possible solution with this decision.
-        if index >= len(coins):
-            return math.inf
+        c1 = float('inf')
         
-        
-        if dp[index][amount] != -1:
-            return dp[index][amount]
-        
-        count1 = math.inf
-        
-        if coins[index] <= amount:
-            res = self.coinChange_rec(coins, amount - coins[index], index, dp)
+        # Try picking the coin
+        if total - coins[index] >= 0:
+            c1 = self.solve(coins, index, total - coins[index], dp)
             
-            if res != math.inf:
-                count1 = res + 1
+            if c1 != float('inf'):
+                c1 += 1
+         
+        # Try skipping the coin
+        c2 = self.solve(coins, index + 1, total, dp)
         
-        count2 = self.coinChange_rec(coins, amount, index + 1, dp)
-        
-        dp[index][amount] = min(count1, count2)
-        return dp[index][amount]
+        # Return the minimum for the two options
+        dp[index][total] = min(c1, c2)
+        return dp[index][total]
             
+'''
+        
         
