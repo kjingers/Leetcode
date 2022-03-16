1832a9a07d66b9c2be1d9c751c22e2e273b8d2db
kjingers <kurtis.ingersoll@gmail.com>
2022-03-16 11:30:36 -0400

    Create CountUniqueCharactersOfAllSubstringsOfAGivenString.py

---
+++ Problems/CountUniqueCharactersOfAllSubstringsOfAGivenString/CountUniqueCharactersOfAllSubstringsOfAGivenString.py
@@ -0,0 +1,36 @@
+'''
+
+Dynamic programming. Default lostPos to -1, for index just before start of array 0.
+
+At each char, we
+    - Subtract previous contributions of char in substring
+    - calc # substrings where char is unique (i - lastPos)
+    - Add new contributions to runningTotal
+    - update new lastPos for char
+    - increment res
+
+'''
+
+# Tracking contribution of each character for each substring ending at i
+class Solution:
+    def uniqueLetterString(self, s: str) -> int:
+        
+        contributions = [0 for _ in range(26)]
+        lastPos = [-1 for _ in range(26)]
+        currTotal = 0
+        res = 0
+        
+        for i, char in enumerate(s):
+            char_idx = ord(char) - ord('A')
+            currTotal -= contributions[char_idx]
+            contributions[char_idx] = i - lastPos[char_idx]
+            currTotal += contributions[char_idx]
+            lastPos[char_idx] = i
+            res += currTotal
+            
+        return res
+
+
+
+        
+        
