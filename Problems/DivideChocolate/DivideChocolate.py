5eb114ef61cd2b95f942dcc9408bfad26a7ca910
kjingers <kurtis.ingersoll@gmail.com>
2022-03-17 12:49:25 -0400

    Update DivideChocolate.py

--- Problems/DivideChocolate/DivideChocolate.py
+++ Problems/DivideChocolate/DivideChocolate.py
@@ -1,45 +1,41 @@
 '''
-Brute Force: Start with speed = 1 and calculate the number of hours it takes to eat all the piles.
-    - If # hours > h, then increment speed
-    - if # hours <= h: return speed
-    
-O(nm) where m is the min speed
+Binary Search Problem:
 
-Binary Search: 
-
-If a given speed s works, then we know all speeds >= s work too. Also, if a speed s doesnt work, then all speeds <= s also don't work. So, we can use binary search to find the boundary - the first speed that works.
-
-The search space are speeds from 1 to max(piles)
-
-So can use binary search. At each iteration:
-    - m = speed to test
-    - Check if mid is a valid speed. If yes, then set right = mid
-    - If not valid speed, set left = mid + 1
-    
-    
-Time: O(nlogm), where m is the max speed (largest pile size)
+Search range is left = min(sweetness), right = sum(sweetness) // (k + 1)
+    - If we don't want to use a running max, then we need to make our mid (left + right) // 2 + 1. This is because, if left and right are next to eachother, then we will look forever if we set left = mid.
 '''
-import math
 
 class Solution:
-    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+    def maximizeSweetness(self, sweetness: List[int], k: int) -> int:
         
-        left = 1
-        right = max(piles)
+        left = min(sweetness)
+        right = sum(sweetness) // (k + 1)
         
-        while left < right:
+        maxSweetness = 0
+        
+        while left <= right:
+            
             mid = left + (right - left) // 2
             
-            hours = 0
-            # Check if this speed works
-            for pile in piles:
-                hours += math.ceil(pile / mid)
-                
-            if hours <= h:
-                right = mid
-            else:
+            if self.checkCuttingPlan(sweetness, mid, k):
+                maxSweetness = max(maxSweetness, mid)
                 left = mid + 1
+            else:
+                right = mid - 1
+                
+        return maxSweetness
         
-        return left
+    def checkCuttingPlan(self, sweetness, minSweetness, k):
+        curr = 0
+        people = 0
+        
+        for s in sweetness:
+            curr += s
             
+            if curr >= minSweetness:
+                people += 1
+                curr = 0
+                
+        return True if people >= (k + 1) else False
+
         
