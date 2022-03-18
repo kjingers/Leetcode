63961df9ef78e62511e06774304d89fb2cd51ac3
kjingers <kurtis.ingersoll@gmail.com>
2022-03-18 12:04:01 -0400

    Create AnalyzeUserWebsiteVisitPattern.py

---
+++ Problems/AnalyzeUserWebsiteVisitPattern/AnalyzeUserWebsiteVisitPattern.py
@@ -0,0 +1,24 @@
+class Solution:
+    def mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> List[str]:
+        
+            
+        z = sorted(zip(username, timestamp, website), key = lambda x : (x[0], x[1]))
+        
+        users = defaultdict(list)
+        
+        for user, time, site in z:
+            users[user].append(site)
+            
+
+        patterns = Counter()
+    
+        # Get 3 site combinations for each user
+        for user, sites in users.items():
+            three_combo = set(combinations(sites, 3))
+            three_combo = Counter(three_combo)
+            patterns.update(three_combo)
+        
+        return max(sorted(patterns), key=patterns.get)
+        
+        
+        
