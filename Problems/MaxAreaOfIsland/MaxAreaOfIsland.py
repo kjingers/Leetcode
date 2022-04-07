be1e4f5c93fde560dce2ab8a293106b97985ce00
kjingers <kurtis.ingersoll@gmail.com>
2022-04-07 09:15:39 -0400

    Update MaxAreaOfIsland.py

--- Problems/MaxAreaOfIsland/MaxAreaOfIsland.py
+++ Problems/MaxAreaOfIsland/MaxAreaOfIsland.py
@@ -1,41 +1,79 @@
 '''
-Definitely a matrix bfs/dfs traversal problem.
-
-Time: O(m*n)
-Space: O(1)
-
-For Iterative BFS, we will want to call a new BFS when we find a 1 in our initial traversal. 
-Whenever we pop from queue, we add 1 to answer. Only put coords in queue if in-bounds and a 1. That way,
-whever we pop, we add 1. When queue becomes empty, the entire island has been processed so we can return area
-of current island.
+Graph traversal problem.
+    - Can edit in-place to avoid need for visited data structure\
+    - Base case: return 0
+    
+Iteative BFS/DFS:
+    - Easiest to perform in a function, so can easily keep track of current area
+    - Must mark cell as 0 when adding to queue.
 '''
 
-# Recursive DFS
+# DFS Recursive
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         
         maxArea = 0
-        m = len(grid)
-        n = len(grid[0])
+        for i in range(len(grid)):
+            for j in  range(len(grid[0])):
+                if grid[i][j] == 1:
+                    maxArea = max(maxArea, self.dfs(grid, i, j))
         
-        for i in range(m):
-            for j in range(n):
-                count = self.dfs(grid, i, j)
-                maxArea = max(maxArea, count)
-                
         return maxArea
-                
+    
     def dfs(self, grid, i, j):
         
+        # Base Condition
         if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] != 1:
             return 0
         
         grid[i][j] = 0
+        currArea = 0
         
-        count = 0
-        
-        for x, y in ((-1, 0), (1, 0), (0, -1), (0, 1)):
-            count += self.dfs(grid, i + x, j + y)
+        for (x, y) in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+            currArea += self.dfs(grid, i + x, j + y)
+            #currMax = max(currMax, self.dfs(grid, i + x, j + y))
             
-        return 1 + count
+        return 1 + currArea
+    
+# BFS Iterative
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        
+        maxArea = 0
+        for i in range(len(grid)):
+            for j in  range(len(grid[0])):
+                if grid[i][j] == 1:
+                    #area = self.bfs(grid, i, j)
+                    #print("i: %d, j: %d, area: %d" % (i, j, area))
+                    maxArea = max(maxArea, self.bfs(grid, i, j))
+        
+        return maxArea
+    
+    def bfs(self, grid, i, j):
+        
+        queue = deque([(i, j)])
+        currArea = 0
+        
+        while queue:
+            x, y = queue.popleft()
+            #print("x: %d, y: %d" % (x, y))
+            currArea += 1
+            grid[x][y] = 0
+            
+            for (dx, dy) in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+                r = x + dx
+                c = y + dy
+                
+                if r < 0 or c < 0 or r >= len(grid) or c >= len(grid[0]) or grid[r][c] != 1:
+                    continue
+                
+                grid[r][c] = 0
+                queue.append((r, c))
+                
+        
+        return currArea
+            
+            
+        
+        
         
